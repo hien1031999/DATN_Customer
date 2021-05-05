1c4273442d2cf48e6e47326ba3d2c3df0a5d7b98
Quyet Ngo <nguynhuan123@gmail.com>
2021-05-05 10:20:16 +0700

    Revert "Nhúng lại temp + trang giỏ hàng + thanh toán + đăng ký + tạo tài khoản"
    This reverts commit 0e39517cf432b251d84cc56292faf9e41dc82470.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +13,6 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-//Gr User
-Route::group(['namespace' => 'User'], function() {
-    Route::get('/trangchu', 'TrangChuController@index')->name('trangchu');
-    Route::get('/sanpham', 'SanPhamController@index')->name('sanpham');
-    Route::get('/tintuc', 'TinTucController@index')->name('tintuc');
-    Route::get('/chitietsanpham', 'CTSPController@index')->name('chitietsanpham');
-    Route::get('/chitiettintuc', 'TinTucController@chitiettintuc')->name('chitiettintuc');
-    Route::get('/dangnhapdangky', 'DangNhapDangKyController@index')->name('dangnhapdangky');
-    Route::get('/giohang', 'GioHangController@index')->name('giohang');
-    Route::get('/thanhtoan', 'ThanhToanController@index')->name('thanhtoan');
+Route::get('/', function () {
+    return view('welcome');
 });
